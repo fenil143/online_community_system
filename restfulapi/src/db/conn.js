b310e565d24586466c641eb236df2c933490e372
Bhavik patel <bhavik5025@gmail.com>
2023-12-28 10:12:30 +0530

    Update conn.js

--- restfulapi/src/db/conn.js
+++ restfulapi/src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/thapa").then(()=>{
+mongoose.connect("mongodb+srv://bhavik_patel:Svsm4142@bhavik.eknxbqt.mongodb.net/").then(()=>{
     console.log("connection successful");
 }).catch((e)=>{
     console.log("No connection");
