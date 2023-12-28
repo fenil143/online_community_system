54b7c6f0a2865fb1dcd29e714d8f6b2687356a45
Bhavik patel <bhavik5025@gmail.com>
2023-12-28 10:57:13 +0530

    atlas_Setup

--- restfulapi/src/app.js
+++ restfulapi/src/app.js
@@ -1,25 +1,32 @@
 const express = require("express");
 require("./db/conn");
-const Student = require("./models/students");
+// const Student = require("./models/students");
+
+const studentApi = require("./routers/student/api");
 const app = express();
 const port = process.env.PORT || 8000;
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
-app.get("/students",(req,res) =>{
-    res.send("Hello world hello ")
-})
-
-app.post("/students", (req,res)=>{
-    // res.send("Hello from the other sides");
-    console.log(req.body.name);
-    const user = new Student(req.body);
-    user.save().then(()=>{
-        res.send(user);
-    }).catch((e)=>{
-        res.send(e);
-    });
-})
+app.use(cors({
+    origin:"http://localhost:8000"
+}
+));
+app.use(studentApi)
+// app.get("/students",(req,res) =>{
+//     res.send("Hello world hello ")
+// })
+// // app.use("studentApi",studentApi);
+// app.post("/students", (req,res)=>{
+//     // res.send("Hello from the other sides");
+//     console.log(req.body.name);
+//     const user = new Student(req.body);
+//     user.save().then(()=>{
+//         res.send(user);
+//     }).catch((e)=>{
+//         res.send(e);
+//     });
+// })
 
 app.listen(port, () => {
     console.log(`connection is setup at ${port}`)
--- restfulapi/src/db/conn.js
+++ restfulapi/src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://bhavik_patel:Svsm4142@bhavik.eknxbqt.mongodb.net/").then(()=>{
+mongoose.connect("mongodb+srv://fenil:Svsm4142@ocms.ggujxsk.mongodb.net/Online_Community_System").then(()=>{
     console.log("connection successful");
 }).catch((e)=>{
     console.log("No connection");
