6080165cc4dde942236187efcc14427e79f4ac7d
Bhavik patel <bhavik5025@gmail.com>
2024-01-18 22:30:51 +0530

    lottie add on registration page

--- online_community_system/src/app/authentication/assets/register_lottie.js
+++ online_community_system/src/app/authentication/assets/register_lottie.js
@@ -1,9 +1,11 @@
-export default function Lottie()
+export default function Register_Lottie()
 {
     return(
         <>
          <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
-         <div className="flex justify-end items-center ">
+
+<dotlottie-player src="https://lottie.host/38390bb9-ec18-4f7b-847a-f512d1d9955c/ilR9zqRME0.json" background="transparent" speed="1"  style={{ width: '400px', height: '500px' }} loop autoplay></dotlottie-player>
+         {/* <div className="flex justify-end items-center ">
         <dotlottie-player
           className="w-[600px] h-[500px]"
           src="https://lottie.host/64a5b794-aa83-40d6-8f7c-b15316abd968/UzmH9hX05V.json"
@@ -13,9 +15,6 @@ export default function Lottie()
           loop
           autoplay
         ></dotlottie-player>
-      </div>
-
-</>
-
-    )
+      </div> */}
+      </>);
 }
