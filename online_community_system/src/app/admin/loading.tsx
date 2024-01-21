6da15f49ced78c699c12df14adf3c192817885fc
fenil143 <mahendrafenil8@gmail.com>
2024-01-21 10:29:42 +0530

    loading page added for admin pages

---
+++ online_community_system/src/app/admin/loading.tsx
@@ -0,0 +1,12 @@
+function loading() {
+    return (
+        <div>
+            <span className="loading loading-infinity loading-xs"></span>
+            <span className="loading loading-infinity loading-sm"></span>
+            <span className="loading loading-infinity loading-md"></span>
+            <span className="loading loading-infinity loading-lg"></span>
+        </div>
+    );
+}
+
+export default loading;
