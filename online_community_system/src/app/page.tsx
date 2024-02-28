e522515e21cf8fe046001937a9287ad687156e68
fenil143 <mahendrafenil8@gmail.com>
2024-02-28 12:16:52 +0530

    automatically render to the admin and student page if localstorage variable is there

--- online_community_system/src/app/page.tsx
+++ online_community_system/src/app/page.tsx
@@ -1,9 +1,34 @@
-import Image from 'next/image'
+// import Image from 'next/image'
+// import './globals.css';
+// import {redirect} from 'next/navigation';
+
+// export default function Home() {
+//   redirect("/landing");
+//   return (
+//     <h3>Welcome to our application</h3>
+//   );
+// }
+'use client'
+import Image from 'next/image';
 import './globals.css';
 import {redirect} from 'next/navigation';
 
 export default function Home() {
+
+  const isAdmin = localStorage.getItem('admin');
+  if (isAdmin) {
+    redirect("/admin");
+    return null; 
+  }
+
+  const isStudent = localStorage.getItem('student');
+  if (isStudent) {
+    redirect("/student");
+    return null; 
+  }
+
   redirect("/landing");
+  // If neither "admin" nor "student" is present, render the welcome message
   return (
     <h3>Welcome to our application</h3>
   );
